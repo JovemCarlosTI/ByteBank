86f7f3e4ba544bd406183aeba1540e8ec582d318
JovemCarlosTI <59931270+JovemCarlosTI@users.noreply.github.com>
2023-06-04 00:11:11 +0000

    Classe abstrata

--- Conta.js
+++ Conta.js
@@ -1,12 +1,13 @@
+// Classe Abstrata
 export class Conta {
 	constructor(saldoInicial, cliente, agencia) {
+		if (this.constructor == Conta) {
+			throw new Error("Você não deveria instanciar um objeto do tipo Conta");
+		}
+		
 		this._saldo = saldoInicial;
 		this._cliente = cliente;
 		this._agencia = agencia;
-
-		if(this.constructor == Conta) {
-			console.log("Você não deveria instanciar um objeto do tipo Conta");
-		}
 	}
 	
 	set cliente(cliente) {
